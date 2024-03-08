c3730b001151fa9da2badc080029d88c8a29b0a9
tahapek5454 <tahapek5454@gmail.com>
2024-03-08 22:09:12 +0300

    feat: add product controller and models

---
+++ ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models.ViewModels;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductVM createProductVM)
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ ProductApp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductApp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string ProductName { get; set; }
+        public int Count { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal Price { get; set; }
+    }
+}
