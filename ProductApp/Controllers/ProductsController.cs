aaef6ee18a72ecb666d9b9b3066c34afbbe48d28
tahapek5454 <tahapek5454@gmail.com>
2024-03-09 12:10:45 +0300

    feat: add createProduct action

--- ProductApp/Controllers/ProductsController.cs
+++ ProductApp/Controllers/ProductsController.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProductApp.Models.ViewModels;
+using Shared.Events;
+using Shared.Services.Abstract;
 
 namespace ProductApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(IEventStoreService _eventStoreService) : ControllerBase
     {
 
         [HttpPost]
         public async Task<IActionResult> CreateProduct([FromBody] CreateProductVM createProductVM)
         {
 
-            return Ok();
+            NewProductAddedEvent @event = new NewProductAddedEvent()
+            {
+                ProductId = Guid.NewGuid().ToString(),
+                InitialCount = createProductVM.Count,
+                InitialPrice = createProductVM.Price,
+                IsAvailable = createProductVM.IsAvailable,
+                ProductName = createProductVM.ProductName,
+            };
+
+            await _eventStoreService.AppendToStreamAsync("product-stream",
+                    new[] { _eventStoreService.GenerateEventData(@event) }
+                );
+
+            return Created();
         }
     }
 }
